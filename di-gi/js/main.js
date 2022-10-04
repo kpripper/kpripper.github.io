1bd64eccf717cee6895a1cab9bff230e6a939f70
kpripper <102256496+kpripper@users.noreply.github.com>
2022-10-04 21:50:19 +0300

    Верхній слайдер

--- di-gi/js/main.js
+++ di-gi/js/main.js
@@ -1,42 +1,59 @@
-const arrowForward = document.querySelector('.arrow-forward')
+window.onload = function () {
+  const backForward = document.querySelector('.arrow-back')
+  const arrowForward = document.querySelector('.arrow-forward')
 
-if (arrowForward) {
   arrowForward.addEventListener('click', carouselForward)
-} else {
-  console.log('arrowForward is null')
-}
-
-const backForward = document.querySelector('.arrow-back')
-
-if (backForward) {
   backForward.addEventListener('click', carouselBack)
-} else {
-  console.log('backForward is null')
+
+  // if (arrowForward) {
+  //   arrowForward.addEventListener('click', carouselForward)
+  //   console.log('arrowForward is')
+  // } else {
+  //   console.log('arrowForward is null')
+  // }
+
+  // if (backForward) {
+  //   backForward.addEventListener('click', carouselBack)
+  //   console.log('backForward is')
+  // } else {
+  //   console.log('backForward is null')
+  // }
 }
 
 function carouselForward() {
-  alert('carouselForward')
   const activeElement = document.querySelector('.active-opacity-carousel')
+  const activeCircle = document.querySelector('.white-circle-border')
   const firstElement = document.querySelector('.opacity-carousel-1')
-  const nextElement = activeElement.nextElementSibling
+  const firstCircle = document.querySelector('.white-circle-1')
+  const nextElementSlider = activeElement.nextElementSibling
+  const nextElementCircle = activeCircle.nextElementSibling
 
   activeElement.classList.remove('active-opacity-carousel')
-  if (nextElement != null) {
-    nextElement.classList.add('active-opacity-carousel')
+  activeCircle.classList.remove('white-circle-border')
+  if (nextElementSlider != null) {
+    nextElementSlider.classList.add('active-opacity-carousel')
+    nextElementCircle.classList.add('white-circle-border')
   } else {
     firstElement.classList.add('active-opacity-carousel')
+    firstCircle.classList.add('white-circle-border')
   }
 }
 
 function carouselBack() {
   const activeElement = document.querySelector('.active-opacity-carousel')
+  const activeCircle = document.querySelector('.white-circle-border')
   const lastElement = document.querySelector('.opacity-carousel-3')
-  const prevElement = activeElement.previousElementSibling
+  const lastElementCircle = document.querySelector('.white-circle-3')
+  const prevElementSlider = activeElement.previousElementSibling
+  const prevElementCircle = activeCircle.previousElementSibling
 
   activeElement.classList.remove('active-opacity-carousel')
-  if (prevElement != null) {
-    prevElement.classList.add('active-opacity-carousel')
+  activeCircle.classList.remove('white-circle-border')  
+  if (prevElementSlider != null) {
+    prevElementSlider.classList.add('active-opacity-carousel')
+    prevElementCircle.classList.add('white-circle-border')
   } else {
     lastElement.classList.add('active-opacity-carousel')
+    lastElementCircle.classList.add('white-circle-border')
   }
 }
